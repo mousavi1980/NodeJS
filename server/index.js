ead46de6c09a310946ffe0d0cf80c266812318fb
mousavi1980 <seyed.morteza.mousavi@gmail.com>
2018-03-07 10:59:33 +1100

    commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
@@ -9,7 +10,33 @@ app.get('/', (req, res) => {
   res.send({ bye : 'buddy' });
 });
 
-//passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+  {
+    clientID : keys.googleClientID,
+    clientSecret : keys.googleCleintSecret,
+    callBackURL : '/auth/google/callback'
+  },
+    accessToken => {
+    console.log(accessToken);
+  }
+  )
+);
 
+app.get(
+  '/auth/goole',
+  passport.authenticate('google',
+  {
+    scope : ['profile','email']
+  })
+);
+
+
+
+
+
+
+
+)
 const PORT = process.env.PORT || 5002;
 app.listen(PORT);
