cee81804c774f03d2e2aa570e52c4475b645f9ab
mousavi1980 <seyed.morteza.mousavi@gmail.com>
2018-03-08 11:20:34 +1100

    Mongoose Installed

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,27 @@
-app.get(
-  '/auth/google',
-  passport.authenticate('google',
-  {
-    scope : ['profile','email']
-  })
-);
+const passport = require('passport');
 
-app.get('/auth/google/callback' , passport.authenticate('google'));
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+};
