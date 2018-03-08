d47cbe1fed433163d2890944737a30e2703faa9f
mousavi1980 <seyed.morteza.mousavi@gmail.com>
2018-03-08 13:33:41 +1100

    Rolleback to before server refactoring

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
   googleClientID : '849686288993-afo9m0u4afhhkio7pe5831lldjv90p3n.apps.googleusercontent.com',
   googleClientSecret : 'RZzRbPu_qoKj9IMi1lmsjsX_',
-  mongoURI : 'mongodb://stephen:password@ds119380.mlab.com:19380/emaily-dev',
 };
--- server/index.js
+++ server/index.js
@@ -1,18 +1,38 @@
 
 const express = require('express');
-const mongoose = require('mongoose');
-require('./services/passport');
-const keys = require('./config/keys.js');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
-
-mongoose.connect(keys.mongoURI);
-
-require('./routes/authRoutes')(app);
 
 app.get('/', (req, res) => {
   res.send({ bye : 'buddy' });
 });
+
+passport.use(
+  new GoogleStrategy(
+  { clientID : keys.googleClientID,
+    clientSecret : keys.googleClientSecret,
+    callbackURL : '/auth/google/callback',
+  },
+    (accessToken,refreshToken, profile, done) => {
+    console.log('accessToken: ',accessToken);
+    console.log('refreshToken: ', refreshToken);
+    console.log('profile: ', profile);
+  }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google',
+  {
+    scope : ['profile','email']
+  })
+);
+
+app.get('/auth/google/callback' , passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
