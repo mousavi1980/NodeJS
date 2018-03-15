dad0b49f06eb2897c2e4f5a1f60bca76d0229077
mousavi1980 <seyed.morteza.mousavi@gmail.com>
2018-03-15 13:40:29 +1100

    css added

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-const Header = () => <h2> Header</h2>
+import Header from './Header';
 const Dashboard = () => <h2> Dashboard </h2>
 const SurveyNew = () => <h2> SurveyNew</h2>
 const Landing = () => <h2> Landing </h2>
@@ -12,11 +12,13 @@ const Landing = () => <h2> Landing </h2>
 const App = () => {
 
     return (
-        <div>
+        <div className="container">
             <BrowserRouter>
                 <div>
+                <Header />
                     <Route exact path="/" component={Landing}/>
-                    <Route path="/surveys" component = {Dashboard}/>
+                    <Route exact path="/surveys" component = {Dashboard}/>
+                    <Route exact path="/surveys/new" component={SurveyNew}/>
 
 
                 </div>
