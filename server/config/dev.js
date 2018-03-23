0414f42b768593170f8fc1a05dc28b0935d8f55f
mousavi1980 <seyed.morteza.mousavi@gmail.com>
2018-03-23 12:50:12 +1100

    commit

--- server/config/dev.js
+++ server/config/dev.js
@@ -3,4 +3,6 @@ module.exports = {
   googleClientSecret : 'RZzRbPu_qoKj9IMi1lmsjsX_',
   mongoURI : 'mongodb://stephen:password@ds261838.mlab.com:61838/emaily-dev2',
   cookieKey : 'klbfkasbdfjbsdkjbvsdjbgrkvbrjksbdfkgdsjbvkjsbarvj',
+  stripePublishableKey : 'pk_test_M4El2xW6R6Mj8TtoIEm215Qa',
+  stripeSecretKey : 'sk_test_E339Q3ex8zofIdt4q8JX9GWB',
 };
